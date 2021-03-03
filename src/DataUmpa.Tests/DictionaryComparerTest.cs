82ca017aad69495dc00b6448b0783ce4dfbd8b0a
Arialdo Martini <arialdomartini@gmail.com>
2021-03-03 11:47:43 +0100

    Original code: DictionaryComparer with a single test

---
+++ src/DataUmpa.Tests/DictionaryComparerTest.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using FluentAssertions;
+using Xunit;
+
+namespace DataUmpa.Tests
+{
+    public class DictionaryComparerTest
+    {
+        [Fact]
+        void combined()
+        {
+            var left = new List<int> {1, /*2,*/ 3, 4}.ToImmutableDictionary(i => i, i => i.ToString());
+            var right = new List<int>{1, 2, 3}.ToImmutableDictionary(i => i, i => i.ToString());
+
+            var executedActions = new List<string>();
+
+            void WhenBoth(string leftItem, string rightItem) =>
+                executedActions.Add($"Both {leftItem}");
+
+            void WhenLeft(string leftItem) =>
+                executedActions.Add($"Left {leftItem}");
+
+            void WhenRight(string rightItem) =>
+                executedActions.Add($"Right {rightItem}");
+
+            DictionaryComparer.Sync(left, right, WhenLeft, WhenRight, WhenBoth);
+
+            executedActions.Should().BeEquivalentTo(
+                new List<string>
+                {
+                    "Both 1",
+                    "Both 3",
+                    "Left 4",
+                    "Right 2"
+                });
+        }
+    }
+}
---
+++ src/DataUmpa/DictionaryComparer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace DataUmpa
+{
+    public static class DictionaryComparer
+    {
+        internal static IEnumerable<Action> GetActions<TKey, TFrom, TTo>(ImmutableDictionary<TKey, TFrom> left,
+            ImmutableDictionary<TKey, TTo> right,
+            Action<TFrom> whenLeft,
+            Action<TTo> whenRight,
+            Action<TFrom, TTo> whenBoth)
+        {
+            Action MakeLeftAndBoth(KeyValuePair<TKey, TFrom> from) =>
+                () => LeftOrBoth(from.Key, from.Value);
+
+            Action MakeRight(KeyValuePair<TKey, TTo> to) =>
+                () => { Right(to.Key, to.Value); };
+
+            void LeftOrBoth(TKey key, TFrom value)
+            {
+                if (right.ContainsKey(key))
+                    whenBoth(value, right[key]);
+                else
+                    whenLeft(value);
+            }
+
+            void Right(TKey key, TTo value)
+            {
+                if (!left.ContainsKey(key))
+                    whenRight(value);
+            }
+
+            var leftAndBoth = left.Select(MakeLeftAndBoth);
+            var rights = right.Select(MakeRight);
+
+            return leftAndBoth.Union(rights);
+        }
+
+        public static void Sync<TKey, TFrom, TTo>(ImmutableDictionary<TKey, TFrom> left,
+            ImmutableDictionary<TKey, TTo> right,
+            Action<TFrom> whenLeft,
+            Action<TTo> whenRight,
+            Action<TFrom, TTo> whenBoth)
+        {
+            GetActions(left, right, whenLeft, whenRight, whenBoth).Run();
+        }
+
+        internal static void Run(this IEnumerable<Action> actions) =>
+            actions.ToList().ForEach(f => f.Invoke());
+    }
+}
